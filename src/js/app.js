7f117b2a0455ae84f29f890b3aea2e77091a08e5
alcfdez <alcfdez@hotmail.com>
2023-02-08 22:22:43 +0100

    init Crud in JS

---
+++ src/js/app.js
@@ -0,0 +1,40 @@
+// const add = () =>{};
+
+// document.querySelector(text);
+// addEventListener("add", add);
+
+// let text =querySelector(text).value
+
+// let save = [];
+
+// save.push( save (add));
+
+
+//Create
+const InputText = document.getElementById('text');
+const btnAddText =document.getElementById('btnAdd');
+
+btnAddText.addEventListener('click');
+
+const textUser = InputText.value;
+
+//me falta enviar los datos a la tabla
+
+//Read
+
+const returnText = (textUser) => {
+const tableUser =document.getElementById('userText');
+return tableUser;
+}
+returnText();
+
+//update
+
+
+
+//delete
+
+const borrar = (userText) => {
+    let index = textUser.findIndex((item, index, array) => {})
+    userText.splice (index,1)
+}
